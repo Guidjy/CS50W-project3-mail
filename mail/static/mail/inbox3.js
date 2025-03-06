fa573d2f564855bc6e56944368fbc4f07bdf3b86
guidjy <guippimentel@gmail.com>
2025-03-06 16:39:20 -0300

    finished view email

--- mail/static/mail/inbox3.js
+++ mail/static/mail/inbox3.js
@@ -16,6 +16,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -29,6 +30,7 @@ function load_mailbox(mailbox) {
   
     // Show the mailbox and hide other views
     document.querySelector('#emails-view').style.display = 'block';
+    document.querySelector('#email-view').style.display = 'none';
     document.querySelector('#compose-view').style.display = 'none';
 
     // Show the mailbox name
@@ -41,8 +43,12 @@ function load_mailbox(mailbox) {
         // render emails
         emails.forEach(email => {
             const emailCard = document.createElement('a');
-            emailCard.href = '#'
-            emailCard.className = 'link-dark text-decoration-none'
+            // this funky syntax is here so "view_email()" has access to the event and email.id
+            emailCard.addEventListener('click', event => {
+                view_email(event, email.id);
+            });
+            emailCard.href = ``;
+            emailCard.className = 'link-dark text-decoration-none';
             emailCard.innerHTML = `
                 <div id="email-${email.id}" class="container-fluid">
                     <div class="row d-flex justify-content-between border-top border-start border-end border-2">
@@ -88,7 +94,8 @@ function send_email(event) {
         body: JSON.stringify({
             recipients: recipients,
             subject: subject,
-            body: body
+            body: body,
+            read: false
         })
     })
     // "response => response.json()" returns the JSON body of the response to the request made above as a promise
@@ -107,4 +114,60 @@ function send_email(event) {
         }
     });
 
+}
+
+
+function view_email(event, email_id) {
+    event.preventDefault();
+
+    // show the email and hide the other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#email-view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
+
+    // delete any previously rendered emails
+    const oldEmail = document.querySelector('#email-content');
+    if (oldEmail !== null) {
+        oldEmail.remove();
+    }
+
+    // makes a get request to "/emails/<email_id>" to request the email
+    fetch(`/emails/${email_id}`)
+    .then(response => response.json())
+    // render the email
+    .then(email => {
+        const emailContent = document.createElement('div');
+        emailContent.id = 'email-content'
+        emailContent.className = 'container';
+        emailContent.innerHTML = `
+            <div class="row d-flex align-items-center mb-3">
+                <div class="col d-flex justify-content-start">
+                    <h1>${email.subject}</h1>
+                </div>
+                <div class="col d-flex justify-content-end">
+                    <p class="fw-light">${email.timestamp}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p class="span">From: ${email.sender}</p>
+                    <p class="fw-light">To: ${email.recipients}</p>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-9 p-5 border mt-5">
+                    <p>${email.body}</p>
+                </div>
+            </div>
+        `;
+        document.querySelector('#email-view').appendChild(emailContent);
+    });
+
+    // makes a PUT request to "/emails/<email_id>" to marek the email as read
+    fetch(`/emails/${email_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
+        })
+      });
 }
